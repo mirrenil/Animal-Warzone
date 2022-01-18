001d04e6345ccec836619cbf3c1687324a7b9475
Jonatan Nyberg <nyberg112@gmail.com>
2022-01-18 13:53:31 +0100

    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com> Co-authored-by: Max Andersson <frontMAX@users.noreply.github.com>

--- src/paused.ts
+++ src/paused.ts
@@ -1,5 +1,6 @@
 class PausedMenu {
-    // private menuState: MenuState;
+    private gameState: GameState;
+    private menuDiv: HTMLDivElement;
     // private gamePaused: Boolean;
     // private continueGameButton: any;
     // private exitGameButton: any;
@@ -7,4 +8,34 @@ class PausedMenu {
     // constructor(menuState: MenuState, gamePaused: Boolean, continueGameButton: any, exitGameButton: any) {
     //     this.menuState = 'paused';
     // }
+
+    constructor(gameState: GameState) {
+        this.gameState = gameState;
+        
+        this.menuDiv = document.createElement('div');
+        this.menuDiv.className = "";
+        
+        const p = document.createElement('p');
+        p.textContent = "Paused";
+        this.menuDiv.append(p);
+
+        const img = document.createElement('img');
+        img.src = "./assets/images/BILD.png";
+
+        const button = document.createElement('button');
+        button.addEventListener('click', this.handleResumeGame);
+    }
+
+    private handleResumeGame() {
+        this.gameState.setGameState("Running");
+        this.closeMenu();
+    }
+    
+    public openMenu() {
+        document.body.append(this.menuDiv);
+    }
+
+    private closeMenu() {
+        this.menuDiv.remove();
+    }
 }
--- src/startMenu.ts
+++ src/startMenu.ts
@@ -1,21 +1,40 @@
 class StartMenu {
-    // private menuState: MenuState;
+    private gameState: GameState;
+    private startMenuDivEl: HTMLDivElement;
     // private width: Number;
     // private height: Number;
     // private appearance: p5.Image;
     // private gameInstructions: Instructions;
 
-    // constructor(menuState: MenuState, width: Number, height: Number, appearance: p5.Image, gameInstructions: Instructions) {
-    //     this.menuState = 'start';
-    // }
+    constructor(gameState: GameState) {
+        this.gameState = gameState;
 
-    // public newGame() {
-        
-    // }
+        this.startMenuDivEl = document.createElement('div');
+        this.startMenuDivEl.className = 'startMenuContainer';
+        this.startMenuDivEl.append('div');
 
-    // public showRules() {
+        const startMenuHeaderEl = document.createElement('h1');
+        startMenuHeaderEl.className = 'startMenuTitle';
+        startMenuHeaderEl.textContent = 'Animal Warzone';
+        this.startMenuDivEl.append('h1');
 
-    // }
+        const newGameButtonEl = document.createElement('button');
+        newGameButtonEl.addEventListener('click', this.startNewGame);
+
+        const instructionsButtonEl = document.createElement('button');
+        instructionsButtonEl.addEventListener('click', this.showInstructionsMenu);
+        this.startMenuDivEl.append('button'); //Skickar denna upp samtiga buttons eller bara den första ????
+    }
+
+    public startNewGame() {
+        this.gameState.setGameState("Character");
+        this.startMenuDivEl.remove();
+    }
+
+    public showInstructionsMenu() {
+        this.gameState.setGameState('Instructions');
+        this.startMenuDivEl.remove();
+    }
 
     // public draw() {
 
