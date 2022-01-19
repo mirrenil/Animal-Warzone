1fa153ef42ca8d367171db5b41d6266af89937bb
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-01-19 15:34:00 +0100

    Did not work to make a PR from another branch. Only added the p5.Images of all characters to sketch and to characters.ts

--- src/sketch.ts
+++ src/sketch.ts
@@ -13,6 +13,22 @@ let wasd: p5.Image;
 let enter: p5.Image;
 let space: p5.Image;
 let esc: p5.Image;
+let rabbitFront: p5.Image;
+let rabbitBack: p5.Image;
+let rabbitLeft: p5.Image;
+let rabbitRight: p5.Image;
+let pigFront: p5.Image;
+let pigBack: p5.Image;
+let pigLeft: p5.Image;
+let pigRight: p5.Image;
+let monkeyFront: p5.Image;
+let monkeyBack: p5.Image;
+let monkeyLeft: p5.Image;
+let monkeyRight: p5.Image;
+let turtleFront: p5.Image;
+let turtleBack: p5.Image;
+let turtleLeft: p5.Image;
+let turtleRight: p5.Image;
 
 
 // let sound: p5.SoundFile
@@ -29,7 +45,7 @@ function preload() {
     pigBig = loadImage('/assets/images/bigPig.png');
     rabbitBig = loadImage('/assets/images/bigRabbit.png');
     turtleBig = loadImage('/assets/images/bigTurtle.png');
-   heart = loadImage('/assets/images/redheart.png')
+    heart = loadImage('/assets/images/redheart.png')
     shield = loadImage('/assets/images/shield.png');
     speed = loadImage('/assets/images/speed.png');
     arrows = loadImage('/assets/images/ArrowKeys.png');
@@ -37,6 +53,26 @@ function preload() {
     enter = loadImage('/assets/images/Enter.png');
     space = loadImage('/assets/images/Space.png');
     esc = loadImage('/assets/images/Escape.png');
+    monkeyBig = loadImage("/assets/images/bigMonkey.png");
+    pigBig = loadImage("/assets/images/bigPig.png");
+    rabbitBig = loadImage("/assets/images/bigRabbit.png");
+    turtleBig = loadImage("/assets/images/bigTurtle.png");
+    rabbitFront = loadImage('/documents/gameCharacters/rabbitFront.png');
+    rabbitBack = loadImage('/documents/gameCharacters/rabbitBack.png');
+    rabbitLeft = loadImage('/documents/gameCharacters/rabbitLeft.png');
+    rabbitRight = loadImage('/documents/gameCharacters/rabbitRight.png');
+    pigFront = loadImage('/documents/gameCharacters/pigFront.png');
+    pigBack = loadImage('/documents/gameCharacters/pigBack.png');
+    pigLeft = loadImage('/documents/gameCharacters/pigLeft.png');
+    pigRight = loadImage('/documents/gameCharacters/pigRight.png');
+    monkeyFront = loadImage('/documents/gameCharacters/monkeyFront.png');
+    monkeyBack = loadImage('/documents/gameCharacters/monkeyBack.png');
+    monkeyLeft = loadImage('/documents/gameCharacters/monkeyLeft.png');
+    monkeyRight = loadImage('/documents/gameCharacters/monkeyRight.png');
+    turtleFront = loadImage('/documents/gameCharacters/turtleFront.png');
+    turtleBack = loadImage('/documents/gameCharacters/turtleBack.png');
+    turtleLeft = loadImage('/documents/gameCharacters/turtleLeft.png');
+    turtleRight = loadImage('/documents/gameCharacters/turtleRight.png');
 }
 
 /**
