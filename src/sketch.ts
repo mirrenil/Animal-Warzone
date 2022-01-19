88d074dc6fb18e3d16977d7dec17aa5ee876b983
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-01-19 14:26:01 +0100

    added all characters to preload and in characters.ts

--- src/sketch.ts
+++ src/sketch.ts
@@ -9,7 +9,26 @@ let wasd: p5.Image;
 let enter: p5.Image;
 let space: p5.Image;
 let esc: p5.Image;
-
+let monkeyBig: p5.Image;
+let pigBig: p5.Image;
+let rabbitBig: p5.Image;
+let turtleBig: p5.Image;
+let rabbitFront: p5.Image;
+let rabbitBack: p5.Image;
+let rabbitLeft: p5.Image;
+let rabbitRight: p5.Image;
+let pigFront: p5.Image;
+let pigBack: p5.Image;
+let pigLeft: p5.Image;
+let pigRight: p5.Image;
+let monkeyFront: p5.Image;
+let monkeyBack: p5.Image;
+let monkeyLeft: p5.Image;
+let monkeyRight: p5.Image;
+let turtleFront: p5.Image;
+let turtleBack: p5.Image;
+let turtleLeft: p5.Image;
+let turtleRight: p5.Image;
 
 // let sound: p5.SoundFile
 
@@ -21,7 +40,7 @@ let esc: p5.Image;
 function preload() {
     // sound: p5.SoundFile = loadSound('../assets/mySound.wav');
     animalWarzon = loadImage('/assets/images/startMenuPicture.png');
-}   heart = loadImage('/assets/images/redheart.png')
+    heart = loadImage('/assets/images/redheart.png')
     shield = loadImage('/assets/images/shield.png');
     speed = loadImage('/assets/images/speed.png');
     arrows = loadImage('/assets/images/ArrowKeys.png');
@@ -29,6 +48,28 @@ function preload() {
     enter = loadImage('/assets/images/Enter.png');
     space = loadImage('/assets/images/Space.png');
     esc = loadImage('/assets/images/Escape.png');
+    monkeyBig = loadImage("/assets/images/bigMonkey.png");
+    pigBig = loadImage("/assets/images/bigPig.png");
+    rabbitBig = loadImage("/assets/images/bigRabbit.png");
+    turtleBig = loadImage("/assets/images/bigTurtle.png");
+    rabbitFront = loadImage('/documents/gameCharacters/rabbitFront.png');
+    rabbitBack = loadImage('/documents/gameCharacters/rabbitBack.png');
+    rabbitLeft = loadImage('/documents/gameCharacters/rabbitLeft.png');
+    rabbitRight = loadImage('/documents/gameCharacters/rabbitRight.png');
+    pigFront = loadImage('/documents/gameCharacters/pigFront.png');
+    pigBack = loadImage('/documents/gameCharacters/pigBack.png');
+    pigLeft = loadImage('/documents/gameCharacters/pigLeft.png');
+    pigRight = loadImage('/documents/gameCharacters/pigRight.png');
+    monkeyFront = loadImage('/documents/gameCharacters/monkeyFront.png');
+    monkeyBack = loadImage('/documents/gameCharacters/monkeyBack.png');
+    monkeyLeft = loadImage('/documents/gameCharacters/monkeyLeft.png');
+    monkeyRight = loadImage('/documents/gameCharacters/monkeyRight.png');
+    turtleFront = loadImage('/documents/gameCharacters/turtleFront.png');
+    turtleBack = loadImage('/documents/gameCharacters/turtleBack.png');
+    turtleLeft = loadImage('/documents/gameCharacters/turtleLeft.png');
+    turtleRight = loadImage('/documents/gameCharacters/turtleRight.png');
+}
+
 
 /**
  * Built in setup function in P5
@@ -37,11 +78,11 @@ function preload() {
  * in the draw function below
  */
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    frameRate(60);
-    // noCursor();
-    
-    game = new Game();
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  // noCursor();
+
+  game = new Game();
 }
 
 /**
@@ -50,24 +91,22 @@ function setup() {
  * you created in the setup function above
  */
 function draw() {
-    background('darkblue');
-    fill('green');
-    stroke('white');
-    strokeWeight(10);
-    circle(width * .5, height * .5, width * 0.2);
-    
+  background("darkblue");
+  fill("green");
+  stroke("white");
+  strokeWeight(10);
+  circle(width * 0.5, height * 0.5, width * 0.2);
 
-    imageMode(CENTER);
-    image(animalWarzon, windowHeight / 2, windowWidth / 2);
+  imageMode(CENTER);
+  image(animalWarzon, windowHeight / 2, windowWidth / 2);
 
-    game.update();
-    game.draw();
+  game.update();
+  game.draw();
 }
-
 
 /**
  *  Built in windowResize listener function in P5
  */
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
