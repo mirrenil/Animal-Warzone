eb053d48787befd4ce14a0d69a1cd7028868cf43
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-01-21 15:39:21 +0100

    fixed the problem with img not loading on startMenu

--- src/menus/startMenu.ts
+++ src/menus/startMenu.ts
@@ -3,7 +3,7 @@ let animalWarzon: p5.Image;
 class StartMenu {
     private gameState: GameState;
     public startMenuDivEl: HTMLDivElement;
-    // private startMenuPicture: HTMLImageElement;
+    private startMenuPicture: HTMLImageElement;
 
     // private width: Number;
     // private height: Number;
@@ -13,30 +13,30 @@ class StartMenu {
     constructor(gameState: GameState) {
         this.gameState = gameState;
 
-    //     this.startMenuDivEl = document.createElement('div');
-    //     this.startMenuDivEl.className = 'startMenuContainer';
+        this.startMenuDivEl = document.createElement('div');
+        this.startMenuDivEl.className = 'startMenuContainer';
 
-    //     // const startMenuPicture = document.createElement('IMG').
-    //     //     startMenuPicture.src = "./assets/images/"
+        this.startMenuPicture = document.createElement('img');
+        this.startMenuPicture.src = '/assets/images/startMenuImg.png';
 
-    //     const startMenuHeaderEl = document.createElement('h1');
-    //     startMenuHeaderEl.className = 'startMenuTitle';
-    //     startMenuHeaderEl.textContent = 'Animal Warzone';
+        const startMenuHeaderEl = document.createElement('h1');
+        startMenuHeaderEl.className = 'startMenuTitle';
+        startMenuHeaderEl.textContent = 'Animal Warzone';
 
-    //     const newGameButtonEl = document.createElement('button');
-    //     newGameButtonEl.addEventListener('click', () => this.startNewGame());
-    //     newGameButtonEl.textContent = 'Starta'
-    //     newGameButtonEl.className = 'btn'
+        const newGameButtonEl = document.createElement('button');
+        newGameButtonEl.addEventListener('click', () => this.startNewGame());
+        newGameButtonEl.textContent = 'Starta'
+        newGameButtonEl.className = 'btn'
 
 
-    //     const instructionsButtonEl = document.createElement('button');
-    //     instructionsButtonEl.addEventListener('click', () => this.showInstructionsMenu());
-    //     instructionsButtonEl.textContent = 'Instruction'
-    //     instructionsButtonEl.className = 'btn'
+        const instructionsButtonEl = document.createElement('button');
+        instructionsButtonEl.addEventListener('click', () => this.showInstructionsMenu());
+        instructionsButtonEl.textContent = 'Instruction'
+        instructionsButtonEl.className = 'btn'
 
-    //     this.startMenuDivEl.append(startMenuHeaderEl, newGameButtonEl, instructionsButtonEl)
-    //     this.openStartMenu();
-    // }
+        this.startMenuDivEl.append(startMenuHeaderEl, this.startMenuPicture, newGameButtonEl, instructionsButtonEl)
+        this.openStartMenu();
+    }
 
 
     public openStartMenu() {
@@ -45,10 +45,10 @@ class StartMenu {
     }
 
 
-    // public startNewGame() {
-    //     this.gameState.setGameState("Character");
-    //     this.startMenuDivEl.remove();
-    // }
+    public startNewGame() {
+        this.gameState.setGameState("Character");
+        this.startMenuDivEl.remove();
+    }
 
 
     public showInstructionsMenu() {
