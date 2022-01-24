00d92e8d3311b674156736b092270d5eb7894136
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-01-24 14:48:51 +0100

    Davids kodhjälp
    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>
    Co-authored-by: Hazem Kawas <hazem-89@users.noreply.github.com>
    Co-authored-by: Miranda Nilhag <mirrenil@users.noreply.github.com>

--- src/entites/barricade.ts
+++ src/entites/barricade.ts
@@ -1,7 +1,3 @@
-
-let barricade: p5.Image;
-let img;
-
 class Barricade {
   //  private size: p5.Vector;
 
@@ -9,63 +5,13 @@ class Barricade {
   //     this.size = size; 
   //   }
   private size: p5.Vector;
-  constructor(size: p5.Vector, barricade: p5.Image, heart: p5.Image) {
+  constructor(x: number, y: number, width: number, height: number) {
     this.size = size;
-    barricade = new p5.Image();
+    super(barricade)
   }
+  
   public draw() {
-
-    const imageX = 35
-    const imageY = 35
-    imageMode(CENTER)
-    //horizontal
-    for (let x = 0; x < 4; x++) {
-      // up left
-      image(barricade, imageX * x + width * .5, height * .15, imageX, imageY)
-      image(barricade, imageX * x + width * .5 + imageX * 5, height * .15 + imageY * 2, imageX, imageY)
-
-      //middle-up left
-      image(barricade, imageX * x + width * .08, height * .5 - imageY * 3, imageX, imageY)
-      //middle-up right
-      image(barricade, imageX * x + width * .92 - imageX * 3, height * .5 - imageY * 3, imageX, imageY)
-
-      //middle-down left
-      image(barricade, imageX * x + width * .08, height * .5 + imageY * 2, imageX, imageY)
-      // middle-down right
-      image(barricade, imageX * x + width * .92 - imageX * 3, height * .5 + imageY * 2, imageX, imageY)
-
-      // middle-down
-      image(barricade, imageX * x + width * .4, height * .7, imageX, imageY)
-      image(barricade, imageX * x + width * .4 + imageX * 4, height * .7 + imageY * 4, imageX, imageY)
-
-    }
-    // vertical
-    for (let y = 0; y < 4; y++) {
-      // upp left
-      image(barricade, width * .5 + imageX * 4, y * imageY + height * .15 - imageY, imageX, imageY)
-
-      //middle left 
-      image(barricade, width * .08 + imageX * 3, y * imageY + height * .5 - imageY * 2, imageX, imageY)
-
-      //middle right 
-      image(barricade, width * .92 - imageX * 3, y * imageY + height * .5 - imageY * 2, imageX, imageY)
-
-      // middle
-      image(barricade, width * .5 - imageX / 2, y * imageY + height * .5 - imageY * 2, imageX, imageY)
-      image(barricade, width * .5 + imageX / 2, y * imageY + height * .5 - imageY * 2, imageX, imageY)
-      // middle-down
-      image(barricade, width * .4 + imageX * 4, y * imageY + height * .7, imageX, imageY)
-
-
-    }
-    for (let y = 0; y < 2; y++) {
-      //  left up
-      image(barricade, width * .3 - imageX / 2, y * imageY + height * .2, imageX, imageY)
-      image(barricade, width * .3 - imageX / 2 + imageX, y * imageY + height * .2, imageX, imageY)
-    }
-
-
-
+    image(barricade, imageX * x + width * .5, height * .15, imageX, imageY)
   }
 
   barricadeShape() {
