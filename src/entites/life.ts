90d31d944d768aa7e2ea01e6c55c36cab4d4ec4b
Max Andersson <maxbjornkga@gmail.com>
2022-01-24 13:49:16 +0100

    characters and life

--- src/entites/life.ts
+++ src/entites/life.ts
@@ -1,18 +1,37 @@
 //---- GLOBAL VARIABLES ----//
 let heart: p5.Image;
-class Life extends Entity {
-    // private showLife: p5.Image;
+let player1: p5.TEXT;
+let player2: p5.TEXT;
 
-//     // healCharacter() {
 
-//     // }
+class Life {
+    private size: p5.Vector;
+    // private playerOneHeartsPosition = new p5.Vector()
 
-//     // updateLife() {
-        
-    // }
-    
-    draw() {
-    imageMode(CENTER);
-    image(heart, windowHeight / 2, windowWidth / 2);
+    constructor(size: p5.Vector) {
+        this.size = size;
+        // this.playerOneHeartsPosition = createVector(width - 150, height * .5 - 30)
+
+    }
+
+    public draw() {
+        const imageX = 30
+        const imageY = 30
+        text('Player 1', width * .02 - 16, 50);
+        textSize(20)
+        fill('white');
+        text('Player 2', width * .92 - 16, 50);
+        textSize(20)
+        fill('white');
+
+        imageMode(CENTER);
+
+        for (let x = 0; x < 4; x++) {
+            image(heart, x * imageX + width * .02, 70, imageX, imageY);
+            image(heart, x * imageX + width * .92, 70, imageX, imageY);
+        }
+
+
     }
 }
+
