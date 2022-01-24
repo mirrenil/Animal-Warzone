0b499a6c52bcf1dad62215f84c2ebb91bf9b55d8
Max Andersson <maxbjornkga@gmail.com>
2022-01-24 13:48:32 +0100

    character and life
    Co-authored-by: Edwin Körner <EdwinKorner@users.noreply.github.com>

--- src/world/gameBoard.ts
+++ src/world/gameBoard.ts
@@ -1,6 +1,6 @@
 
 let rectX1: number = 600;
-let rectY1: number =  300;
+let rectY1: number = 300;
 let rectWidth: number = 100;
 let rectHeight: number = 100;
 let move = 5;
@@ -8,18 +8,20 @@ let move = 5;
 
 class GameBoard {
     private gameState: GameState;
+    private life: Life;
     // private worldMap: WorldMap;
     // public character: Character;
     // private entities: Entity[];
     // private playerOne: Character;
     // private playerTwo: Character;
-    
-    
+
+
 
     constructor(gameState: GameState) {
         this.gameState = gameState;
-       
-     }
+        this.life = life;
+
+    }
 
     // private windowBounderies() {
     //     if(rectX1 < 0) {
@@ -63,29 +65,29 @@ class GameBoard {
     // }
 
     public update() {
-     
-        
+
+
     }
 
-    // public draw() {
-    //     this.gameState.setGameState('Running');
-    //     // keyPressed();
-    //     // this.windowBounderies();
-    //     rectMode(CENTER);
-    //     noStroke();
-    //     fill(255);
+    public draw() {
+        // this.gameState.setGameState('Running');
+        // // keyPressed();
+        // // this.windowBounderies();
+        // rectMode(CENTER);
+        // noStroke();
+        // fill(255);
 
-    //     // character test
-    //     rect(rectX1, rectY1, rectWidth, rectHeight);
+        // // character test
+        // rect(rectX1, rectY1, rectWidth, rectHeight);
 
-    // }
+    }
 
     public openGameBoard() {
-       
+
     }
 
     public openGame() {
-        
-        
+
+
     }
 }
