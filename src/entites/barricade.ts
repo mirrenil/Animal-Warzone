b04637a734017748a13f1a14f8d6237ea49526bd
Max Andersson <maxbjornkga@gmail.com>
2022-01-25 13:31:12 +0100

    playerko..
    Co-authored-by: Edwin Körner <EdwinKorner@users.noreply.github.com>
    Co-authored-by: Hazem Kawas <hazem-89@users.noreply.github.com>
    Co-authored-by: Jonatan Nyberg <nybbe123@users.noreply.github.com>

--- src/entites/barricade.ts
+++ src/entites/barricade.ts
@@ -4,12 +4,14 @@ class Barricade {
   //   constructor(size: p5.Vector) {
   //     this.size = size; 
   //   }
+  public x: p5.Vector;
+  private imageX: p5.Image;
   private size: p5.Vector;
-  constructor(x: number, y: number, width: number, height: number) {
-    this.size = size;
-    super(barricade)
+  constructor(x: number, y: number, width: number, height: number, imageX: p5.Image) {
+    // this.size = size;
+    // super(barricade)
   }
-  
+
   public draw() {
     image(barricade, imageX * x + width * .5, height * .15, imageX, imageY)
   }
